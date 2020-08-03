1b7c7b4554f7e77260b1b5ec14e3834948b09979
Parker Williamson <parker.williamson@gmail.com>
2020-08-03 05:04:51 +0000

    Automatically backed up by Learn

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,14 @@
+class ProductsController < ApplicationController
+    def index
+        @cart = cart
+    end
+
+    def add
+    end
+
+    def create
+        cart << params[:product]
+        redirect_to products_path
+    end
+
+end
